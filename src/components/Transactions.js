17c059f528aa39aa63f7cad455cb3759e54f07a6
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-20 20:54:46 -0500

    Added createTransaction and added it to Transactions.js

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router';
 import Transaction from './Transaction';
+import TransactionsModel from '../models/Transactions';
 
 const Transactions = (props) => {
+    const [on, setOn] = useState(false);
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        TransactionsModel.createTransaction(inputs)
+            .then((result) => {
+                console.log(result);
+            })
+        setOn(false);
+        props.history.push('/');
+    }
+
     return (
         <div>
+            {!on && <button onClick={() => setOn(true)}>New Transaction</button>}
+            {on && (
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="name">Name:</label>
+                    <input name="name" type="text" onChange={handleChange} />
+                    <label htmlFor="amount">Amount:</label>
+                    <input name="amount" type="number" step="0.01" onChange={handleChange} />
+                    <button type="submit">Add Transaction</button>
+                </form>
+            )}
             {props.transactions.map(transaction => <Transaction transaction={transaction} />)}
         </div>
     )
 }
 
-export default Transactions;
+export default withRouter(Transactions);
--- src/models/Transactions.js
+++ src/models/Transactions.js
@@ -5,6 +5,17 @@ class TransactionsModel {
         return fetch(url)
             .then((response) => response.json());
     };
+
+    static createTransaction = (transaction) => {
+        return fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(transaction)
+        })
+            .then((response) => response.json())
+    };
 }
 
 export default TransactionsModel;
